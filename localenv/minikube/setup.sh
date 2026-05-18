5367685f3071459daac49683245122979451a34e
Kamuri Amorim <myk.gata14@gmail.com>
2026-05-18 08:36:45 -0300

    fix: update argocd umbrella chart before deploy

--- localenv/minikube/setup.sh
+++ localenv/minikube/setup.sh
@@ -9,6 +9,11 @@ echo "Setting up ArgoCD helm charts"
 helm repo add argo-cd https://argoproj.github.io/argo-helm
 helm repo update
 
+echo "Updating ArgoCD umbrella chart"
+cd ../argo-cd/repo/charts/argocd/
+helm dependency update
+cd - > /dev/null
+
 echo "Deploying ArgoCD"
 helm upgrade --install argo-cd ../argo-cd/repo/charts/argocd/ --namespace argocd --create-namespace
 kubectl delete secret -n argocd -l "owner=helm,name=argo-cd"
